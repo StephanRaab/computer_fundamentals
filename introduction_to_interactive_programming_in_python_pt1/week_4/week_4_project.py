7291fda04fa13850fdb37a3d76de0ed5c6926bc9
Stephan Raab <sarcartist@gmail.com>
2017-08-27 22:15:47 -0400

    paddle 1 and paddle 2 can both move now, have to work on paddle collision now

--- introduction_to_interactive_programming_in_python_pt1/week_4/week_4_project.py
+++ introduction_to_interactive_programming_in_python_pt1/week_4/week_4_project.py
@@ -1,99 +1,3 @@
-#version 1
-
-# # Implementation of classic arcade game Pong
-#
-# import simplegui
-# import random
-#
-# # initialize globals - pos and vel encode vertical info for paddles
-# WIDTH = 600
-# HEIGHT = 400
-# BALL_RADIUS = 20
-# PAD_WIDTH = 8
-# PAD_HEIGHT = 80
-# HALF_PAD_WIDTH = PAD_WIDTH / 2
-# HALF_PAD_HEIGHT = PAD_HEIGHT / 2
-# LEFT = False
-# RIGHT = True
-# ball_pos = [0, 0]
-# ball_vel = [0, 0]
-# paddle1_pos = [HALF_PAD_WIDTH, HALF_PAD_HEIGHT]
-# paddle2_pos = [WIDTH, ]
-# paddle_speed = 0.5
-# paddle1_vel = 0
-# paddle2_vel = 0
-# score_1 = 0
-# score_2 = 0
-#
-#
-# # initialize ball_pos and ball_vel for new bal in middle of table
-# # if direction is RIGHT, the ball's velocity is upper right, else upper left
-# def spawn_ball(direction):
-#     global ball_pos, ball_vel  # these are vectors stored as lists
-#     ball_pos = [WIDTH / 2, HEIGHT / 2]
-#     ball_vel = [0, 0]
-#     if direction == RIGHT:
-#         ball_vel[0] = 1
-#     else:
-#         ball_vel[0] = -1
-#
-#
-# # define event handlers
-# def new_game():
-#     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
-#     global score1, score2  # these are ints
-#     spawn_ball(RIGHT)
-#
-#
-# def draw(canvas):
-#     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
-#
-#     # draw mid line and gutters
-#     canvas.draw_line([WIDTH / 2, 0], [WIDTH / 2, HEIGHT], 1, "White")
-#     canvas.draw_line([PAD_WIDTH, 0], [PAD_WIDTH, HEIGHT], 1, "White")
-#     canvas.draw_line([WIDTH - PAD_WIDTH, 0], [WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
-#
-#     # update ball
-#
-#     # draw ball
-#     canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "white", "white")
-#
-#     # update paddle's vertical position, keep paddle on the screen
-#
-#     # draw paddle
-#     paddle1 = canvas.draw_polygon([[0, 0], [0, PAD_HEIGHT]], PAD_WIDTH, "white", "white")
-#     paddle2 = canvas.draw_polygon([[WIDTH, 0], [WIDTH, PAD_HEIGHT]], PAD_WIDTH, "white", "white")
-#
-#
-#     # determine whether paddle and ball collide
-#
-#     # draw scores
-#
-#
-# def keydown(key):
-#     global paddle1_vel, paddle2_vel, paddle_speed
-#     paddle1_vel += paddle_speed
-#     paddle2_vel += paddle_speed
-#
-#
-# def keyup(key):
-#     global paddle1_vel, paddle2_vel
-#     paddle1_vel = 0
-#     paddle2_vel = 0
-#
-#
-# # create frame
-# frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
-# frame.set_draw_handler(draw)
-# frame.set_keydown_handler(keydown)
-# frame.set_keyup_handler(keyup)
-#
-# # start frame
-# new_game()
-# frame.start()
-
-
-#version 2
 # Implementation of classic arcade game Pong
 
 import simplegui
@@ -116,6 +20,15 @@ score2 = 0
 horiz_vel = random.randrange(120, 240)
 vert_vel = random.randrange(60, 180)
 ball_vel = [horiz_vel / 20, vert_vel / 20]
+paddle1_vel = [0, 0]
+paddle2_vel = [0, 0]
+vel_incr = 5
+
+paddle1_top_pos = [PAD_WIDTH / 2, 0]
+paddle1_bottom_pos = [PAD_WIDTH / 2, PAD_HEIGHT]
+
+paddle2_top_pos = [WIDTH - PAD_WIDTH / 2, 0]
+paddle2_bottom_pos = [WIDTH - PAD_WIDTH / 2, PAD_HEIGHT]
 
 
 # initialize ball_pos and ball_vel for new bal in middle of table
@@ -137,7 +50,7 @@ def new_game():
 
 def draw(canvas):
     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel, test_vel, BALL_RADIUS
-
+    global paddle1_top_pos, paddle1_bottom_pos
     # draw mid line and gutters
     canvas.draw_line([WIDTH / 2, 0], [WIDTH / 2, HEIGHT], 1, "White")
     canvas.draw_line([PAD_WIDTH, 0], [PAD_WIDTH, HEIGHT], 1, "White")
@@ -167,10 +80,17 @@ def draw(canvas):
     canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "white", "white")
 
     # update paddle's vertical position, keep paddle on the screen
+    paddle1_top_pos[1] += paddle1_vel[1]
+    paddle1_bottom_pos[1] += paddle1_vel[1]
+    paddle2_top_pos[1] += paddle2_vel[1]
+    paddle2_bottom_pos[1] += paddle2_vel[1]
 
     # draw paddles
+    paddle1 = canvas.draw_line(paddle1_top_pos, paddle1_bottom_pos, PAD_WIDTH, "White")
+    paddle2 = canvas.draw_line(paddle2_top_pos, paddle2_bottom_pos, PAD_WIDTH, "White")
 
     # determine whether paddle and ball collide
+
 
     # draw scores
     canvas.draw_text(str(score1), [WIDTH / 2.5, 75], 60, "White")
@@ -178,11 +98,27 @@ def draw(canvas):
 
 
 def keydown(key):
-    global paddle1_vel, paddle2_vel
+    global paddle1_vel, paddle2_vel, vel_incr
+    if key == simplegui.KEY_MAP["w"]:
+        paddle1_vel[1] = - vel_incr
+    if key == simplegui.KEY_MAP["s"]:
+        paddle1_vel[1] = vel_incr
+    if key == simplegui.KEY_MAP["up"]:
+        paddle2_vel[1] = - vel_incr
+    if key == simplegui.KEY_MAP["down"]:
+        paddle2_vel[1] = vel_incr
 
 
 def keyup(key):
     global paddle1_vel, paddle2_vel
+    if key == simplegui.KEY_MAP["w"]:
+        paddle1_vel[1] = 0
+    if key == simplegui.KEY_MAP["s"]:
+        paddle1_vel[1] = 0
+    if key == simplegui.KEY_MAP["up"]:
+        paddle2_vel[1] = 0
+    if key == simplegui.KEY_MAP["down"]:
+        paddle2_vel[1] = 0
 
 
 # create frame
