e829e6ef94d7e1e61a1335484e7d25eec8c3a6e0
Stephan Raab <sarcartist@gmail.com>
2017-08-30 22:57:08 -0400

    Ball grid solution, for loop in a for loop

--- introduction_to_interactive_programming_in_python/week5/week5_a_exec.py
+++ introduction_to_interactive_programming_in_python/week5/week5_a_exec.py
@@ -126,3 +126,26 @@ print(string_list_join(["a", "b", "c", "d"]))
 #abcd
 
 print("======================================================")
+# Ball grid slution
+###################################################
+
+BALL_RADIUS = 20
+GRID_SIZE = 10
+WIDTH = 2 * GRID_SIZE * BALL_RADIUS
+HEIGHT = 2 * GRID_SIZE * BALL_RADIUS
+
+
+# define draw
+def draw(canvas):
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            canvas.draw_circle([2 * BALL_RADIUS * i + BALL_RADIUS, 2 * BALL_RADIUS * j + BALL_RADIUS], BALL_RADIUS, 1,
+                               "white", "white")
+
+
+# create frame and register handlers
+frame = simplegui.create_frame("Ball grid", WIDTH, HEIGHT)
+frame.set_draw_handler(draw)
+
+# start frame
+frame.start()
