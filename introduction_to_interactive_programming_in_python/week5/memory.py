af0edab32d0fd2107c94b162225f2a168896afb8
Stephan Raab <sarcartist@gmail.com>
2018-01-14 17:08:56 -0500

    added exposed list and mouse click now compares numbers to see if your guess is right

--- introduction_to_interactive_programming_in_python/week5/memory.py
+++ introduction_to_interactive_programming_in_python/week5/memory.py
@@ -1,7 +1,8 @@
-# implementation of card game - Memory
+# Card game - Memory
 
 import simplegui
 import random
+import math
 
 TOTALCARDS = 16
 CARD_COLOR = "green"
@@ -15,30 +16,61 @@ cardnum2 = range(0,TOTALCARDS//2)
 cardnum.extend(cardnum2)
 random.shuffle(cardnum)
 exposed_list = []
+comparison = []
 
 print "Cards: ", cardnum
 
 # helper function to initialize globals
 def new_game():
+    global state
+    state = 0
     pass  
 
-     
 # define event handlers
 def mouseclick(pos):
     # add game state logic here
-    
-    pass
+    global exposed_list, comparison, state
+    if state == 0:
+        state = 1
+    elif state == 1:
+        state = 2
+    else:
+        state = 1
+        
+    position = int(math.floor(pos[0] / 50.0))
+    comparison.append(position)
+    print comparison
+    if len(comparison) > 1:
+        if cardnum[comparison[0]] == cardnum[comparison[1]]:
+            print "they're the same"
+            exposed_list.append(comparison[0])
+            exposed_list.append(comparison[1])
+            print "exposed", exposed_list
+            comparison = []
+        else:
+            print "wrong"
+            print "exposed", exposed_list
+            comparison = []
                         
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
     init_card = 13
     card_start = 0
-    for i in cardnum:
-        canvas.draw_text(str(cardnum[i]), (init_card, HEIGHT//1.6), 40, "White") 
-        canvas.draw_polygon([(card_start, 0), (card_start + 50, 0),
-                             (card_start + 50,HEIGHT), (card_start, HEIGHT)], 2, CARD_BORDER, CARD_COLOR)
+    for num in cardnum:
+        canvas.draw_text(str(num), (init_card, HEIGHT//1.6), 40, "White")
+        
+        #don't show back of exposed cards
+        if not int(math.floor(card_start/50)) in exposed_list:
+            canvas.draw_polygon([(card_start, 0), (card_start + 50, 0),
+                                 (card_start + 50,HEIGHT), (card_start, HEIGHT)], 2, CARD_BORDER, CARD_COLOR)
+#        #don't show back when in comparison state
+#        if not int(math.floor(card_start/50)) in comparison:
+#            canvas.draw_polygon([(card_start, 0), (card_start + 50, 0),
+#                                 (card_start + 50,HEIGHT), (card_start, HEIGHT)], 2, CARD_BORDER, CARD_COLOR)
+        
         init_card += 50
         card_start += 50
+
 
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", WIDTH, HEIGHT)
@@ -52,6 +84,5 @@ frame.set_draw_handler(draw)
 # get things rolling
 new_game()
 frame.start()
-
 
 # Always remember to review the grading rubric
