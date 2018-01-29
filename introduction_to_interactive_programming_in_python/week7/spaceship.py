d6980cd0ffbbe3f0fd055f8065974f0585db2df8
Stephan Raab <sarcartist@gmail.com>
2018-01-28 20:31:09 -0500

    rock rotation with 2 step

--- introduction_to_interactive_programming_in_python/week7/spaceship.py
+++ introduction_to_interactive_programming_in_python/week7/spaceship.py
@@ -230,7 +230,7 @@ def rock_spawner():
     global a_rock
     random_vel = [random.randrange(-10, 10), random.randrange(-10, 10)]
     random_pos = [random.randrange(WIDTH), random.randrange(HEIGHT)]
-    random_angle_vel = float(random.randrange(-5.0, 5.0)) / 100
+    random_angle_vel = float(random.randrange(-5.0, 5.0, 2)) / 100
     a_rock = Sprite(random_pos, random_vel, 0, random_angle_vel, asteroid_image, asteroid_info)
     
 # initialize frame
