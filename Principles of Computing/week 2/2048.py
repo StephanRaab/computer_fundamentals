6a3695f1194bf87815124f51620e91dabf0733a8
Stephan Raab <sarcartist@gmail.com>
2018-02-18 17:12:03 -0500

    generate new tile, set_tile, get_tile, reset, and str function

--- Principles of Computing/week 2/2048.py
+++ Principles of Computing/week 2/2048.py
@@ -3,6 +3,7 @@ Clone of 2048 game.
 """
 
 import poc_2048_gui
+import random
 
 # Directions, DO NOT MODIFY
 UP = 1
@@ -21,8 +22,30 @@ def merge(line):
     """
     Helper function that merges a single row or column in 2048
     """
-    # replace with your code from the previous mini-project
-    return []
+    first_list = []
+    for num in line:
+        if num  > 0:
+            first_list.append(num)
+    if len(first_list) < len(line):
+        add_zero = len(line) - len(first_list)
+        for num in range(add_zero):
+            first_list.append(0)
+         
+    for num in range(1, len(line)):
+        if first_list[num - 1] == first_list[num]:
+            first_list[num - 1] *= 2
+            first_list[num] = 0
+    
+    final_list = []
+    for num in first_list:
+        if num > 0:
+            final_list.append(num)
+    if len(final_list) < len(line):
+        add_zero = len(line) - len(final_list)
+        for num in range(add_zero):
+            final_list.append(0)
+            
+    return final_list
 
 class TwentyFortyEight:
     """
@@ -33,22 +56,24 @@ class TwentyFortyEight:
         # replace with your code
         self.height = grid_height
         self.width = grid_width
+        self.reset()
 
     def reset(self):
         """
-        Reset the game so the grid is empty except for two
+        Reset the game so the grid is empty, generate two
         initial tiles.
         """
-        # replace with your code
-        pass
+        self.grid = [[0 for col in range(self.get_grid_width())] 
+                   for row in range(self.get_grid_height())]
+        self.new_tile()
+        self.new_tile()
 
     def __str__(self):
         """
         Return a string representation of the grid for debugging.
         """
-        # replace with your code
-        return ""
-
+        return "\n".join([str(row) for row in self.grid])
+        
     def get_grid_height(self):
         """
         Get the height of the board.
@@ -75,22 +100,29 @@ class TwentyFortyEight:
         square.  The tile should be 2 90% of the time and
         4 10% of the time.
         """
-        # replace with your code
-        pass
+        if random.random() <= 0.9:
+            new_tile_num = 2
+        else:
+            new_tile_num = 4
+        
+        open_tiles = [(row, col) for row in range(self.get_grid_height())
+                       for col in range(self.get_grid_width()) if self.get_tile(row,col) == 0]
+                                        
+        if len(open_tiles) != 0:
+            (row, col) = random.choice(open_tiles)
+            self.set_tile(row, col, new_tile_num)
 
     def set_tile(self, row, col, value):
         """
         Set the tile at position row, col to have the given value.
         """
-        # replace with your code
-        pass
+        self.grid[row][col] = value
 
     def get_tile(self, row, col):
         """
         Return the value of the tile at position row, col.
         """
         # replace with your code
-        return 0
-
-
+        return self.grid[row][col]
+    
 poc_2048_gui.run_gui(TwentyFortyEight(5, 4))
