6ae7ebf99bf84f46d99e5c751e2b055dd7e59ac3
Stephan Raab <sarcartist@gmail.com>
2018-02-18 22:29:55 -0500

    testing

--- Principles of Computing/week 2/2048_merge_test.py
+++ Principles of Computing/week 2/2048_merge_test.py
@@ -18,10 +18,10 @@ OFFSETS = {UP: (1, 0),
            LEFT: (0, 1),
            RIGHT: (0, -1)}
 
-T_BORDER = []
-B_BORDER = []
-L_BORDER = []
-R_BORDER = []
+top_border = []
+bottom_border = []
+left_border = []
+right_border = []
 
 def merge(line):
     """
@@ -58,23 +58,23 @@ class TwentyFortyEight:
     """
     def __init__(self, grid_height, grid_width):
         # replace with your code
-        self._height = grid_height
-        self._width = grid_width
+        self.height = grid_height
+        self.width = grid_width
         self.reset()
-        for col in range(self._width):
-            T_BORDER.append([0, col])
+        for col in range(self.width):
+            top_border.append([0, col])
 
-        for col in range(self._width):
-            B_BORDER.append([self._height - 1, col])
+        for col in range(self.width):
+            bottom_border.append([self.height - 1, col])
 
-        for row in range(self._height):
-            L_BORDER.append([row, 0])
+        for row in range(self.height):
+            left_border.append([row, 0])
 
-        for row in range(self._height):
-            R_BORDER.append([row, self._width - 1])
+        for row in range(self.height):
+            right_border.append([row, self.width - 1])
         
-        self._border = {UP: T_BORDER, DOWN: B_BORDER,
-                       LEFT: L_BORDER, RIGHT: R_BORDER}
+        self.border = {UP: top_border, DOWN: bottom_border,
+                       LEFT: left_border, RIGHT: right_border}
 
     def reset(self):
         """
@@ -82,8 +82,8 @@ class TwentyFortyEight:
         initial tiles.
         """
         # set entire grid to zeros
-        self._grid = [[0 for num in range(self.get_grid_width())] 
-                   for num in range(self.get_grid_height())]
+        self.grid = [[0 for col in range(self.get_grid_width())] 
+                   for row in range(self.get_grid_height())]
         
         # generate two new tiles
         self.new_tile()
@@ -93,35 +93,36 @@ class TwentyFortyEight:
         """
         Return a string representation of the grid for debugging.
         """
-        return "\n".join([str(row) for row in self._grid])
+        return "\n".join([str(row) for row in self.grid])
         
     def get_grid_height(self):
         """
         Get the height of the board.
         """
-        return self._height
+        return self.height
 
     def get_grid_width(self):
         """
         Get the width of the board.
         """
-        return self._width
+        return self.width
 
     def move(self, direction):
         """
         Move all tiles in the given direction and add
         a new tile if any tiles moved.
         """
-        init_tiles = self._border[direction]
+        num_steps = 0
+        init_tiles = self.border[direction]
         rows = []
         columns = []
 
-        #RIGHT
-        if OFFSETS[direction] == (0,-1) or OFFSETS[direction] == (0,1):
+        if OFFSETS[direction] == (0,1) or OFFSETS[direction] == (0,-1):
             num_steps = self.get_grid_width()
             for row in init_tiles:
                 for col in range(num_steps):
-                    rows.append([row[0], col])         
+                    rows.append([row[0], col])
+
         else:
             num_steps = self.get_grid_height()
             for row in init_tiles:
@@ -130,47 +131,13 @@ class TwentyFortyEight:
         
         if len(rows) > 0:
             print "rows: ", rows
-            temp1 = []
-            temp2 = []
-            temp3 = []
-            temp4 = []
-            temp5 = []
-            row1 = rows[0:4]
-            row2 = rows[4:8]
-            row3 = rows[8:12]
-            row4 = rows[12:16]
-            row5 = rows[16:20]
-            print row1
-            print row2
-            print row3
-            print row4
-            print row5
-            for num in row1:
-                temp1.append(self._grid[num[0]][num[1]])
-            for num in row2:    
-                temp2.append(self._grid[num[0]][num[1]])
-            for num in row3:    
-                temp3.append(self._grid[num[0]][num[1]])
-            for num in row4:    
-                temp4.append(self._grid[num[0]][num[1]])
-            for num in row5:    
-                temp5.append(self._grid[num[0]][num[1]])
-                
-            print temp1
-            print temp2
-            print temp3
-            print temp4
-            print temp5
-            
-            print "============"
-            
-            print merge(temp1)
-            print merge(temp2)
-            print merge(temp3)
-            print merge(temp4)
-            print merge(temp5)
         else:
             print "columns: ", columns
+
+        print self.grid[0][0]
+        print self.grid[1][0]
+        print self.grid[2][0]
+        print self.grid[3][0]
         
     def new_tile(self):
         """
@@ -194,31 +161,12 @@ class TwentyFortyEight:
         """
         Set the tile at position row, col to have the given value.
         """
-        self._grid[row][col] = value
+        self.grid[row][col] = value
 
     def get_tile(self, row, col):
         """
         Return the value of the tile at position row, col.
         """
-        return self._grid[row][col]
+        return self.grid[row][col]
     
-my_game = TwentyFortyEight(5, 4)
-#my_game.reset()
-#my_game.set_tile(0, 0, 2)
-#my_game.set_tile(1, 0, 2)
-#my_game.set_tile(0, 1, 2)
-#my_game.set_tile(2, 1, 2)
-#my_game.set_tile(0, 2, 2)
-#my_game.set_tile(2, 2, 2)
-#my_game.set_tile(1, 3, 2)
-#my_game.set_tile(2, 3, 2)
-#my_game.set_tile(2, 2, 4)
-#my_game.set_tile(1, 2, 4)
-#my_game.set_tile(1, 1, 4)
-#print str(my_game)
-#print my_game.get_tile(1, 2)
-#my_game.new_tile()
-#my_game.move(RIGHT)
-print str(my_game)    
-    
-#poc_2048_gui.run_gui(TwentyFortyEight(5, 4))
+poc_2048_gui.run_gui(TwentyFortyEight(5, 4))
