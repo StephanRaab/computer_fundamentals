69fed356d901ab83278173c7964c53f930268154
Stephan Raab <sarcartist@gmail.com>
2018-04-15 16:58:57 -0400

    week 7 with some quiz stuff sprinkled in

---
+++ Principles of Computing/week 7/HW_3.py
@@ -0,0 +1,9 @@
+def multiply_up(n):
+    if n == 0:
+        return 1
+    else:
+        return n * multiply_up(n - 1)
+    
+print multiply_up(3)
+
+print 3 * ((3-1))
---
+++ Principles of Computing/week 7/HW_5.py
@@ -0,0 +1,11 @@
+def memoized_fib(num, memo_dict):
+    if num in memo_dict:
+        return memo_dict[num]
+    else:
+        sum1 = memoized_fib(num - 1, memo_dict)
+        sum2 = memoized_fib(num - 2, memo_dict)
+        memo_dict[num] = sum1 + sum2
+        return sum1 + sum2
+    
+print memoized_fib(10, {0 : 0, 1 : 1})
+print (num * 2) -1
